7ca3ab3e4a7157313f4d397e73b20918c8b7ed7e
Anurag Bharti <anuragvasubharti@gmail.com>
2025-02-19 16:14:33 +0530

    Added code

---
+++ src/main/java/com/example/springboothtmlapp/SpringBootGYMHtmlAppApplication.java
@@ -0,0 +1,11 @@
+package com.example.springboothtmlapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGYMHtmlAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGYMHtmlAppApplication.class, args);
+	}
+}
