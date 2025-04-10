656a86bf9ad6035115e1fb3a533f2e936ab1a3ba
Anurag Bharti <anuragvasubharti@gmail.com>
2025-04-11 00:38:11 +0530

    Updated file

--- src/main/java/com/example/springboothtmlapp/controller/HomeController.java
+++ src/main/java/com/example/springboothtmlapp/controller/HomeController.java
@@ -1,12 +1,14 @@
-package com.example.springboothtmlapp;
+package com.example.springboothtmlapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+	
 	@GetMapping("/")
 	public String home() {
-		return "static/Home";
+		return "Home";
 	}
+	
 }
