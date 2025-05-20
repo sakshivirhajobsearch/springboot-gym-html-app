8eac9ec8b4f1ea60bad804af285dc5a862719ba2
sakshivirhajobsearch <164556079+sakshivirhajobsearch@users.noreply.github.com>
2025-05-21 02:40:55 +0530

    Update SpringBootGYMHtmlAppApplication.java

--- src/main/java/com/example/springboothtmlapp/SpringBootGYMHtmlAppApplication.java
+++ src/main/java/com/example/springboothtmlapp/SpringBootGYMHtmlAppApplication.java
@@ -3,9 +3,15 @@ package com.example.springboothtmlapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RestController
 @SpringBootApplication
-public class SpringBootGYMHtmlAppApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootGYMHtmlAppApplication.class, args);
-	}
+public class TestApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApp.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
 }
