b86ea55c9e1d6228334071b87902e3ba72900513
sakshivirhajobsearch <164556079+sakshivirhajobsearch@users.noreply.github.com>
2025-05-21 05:19:59 +0530

    reverted

--- src/main/java/com/example/springboothtmlapp/controller/HomeController.java
+++ src/main/java/com/example/springboothtmlapp/controller/HomeController.java
@@ -1,13 +1,13 @@
 package com.example.springboothtmlapp.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
 	@GetMapping("/")
 	public String home() {
-		return "Home"; // Loads home.html from templates folder
+		return "home"; // Loads home.html from templates folder
 	}
 }
